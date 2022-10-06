7f480fbfabe3cc94e79d7d1b232207d655f684a7
Mike <mishahorod@gmail.com>
2022-10-06 21:46:49 +0300

    commit with first page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+const burger = document.querySelector('#header__burger'),
+      body = document.querySelector('body'),
+      menuNav = document.querySelector('#header-menu__navbar');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menuNav.classList.toggle('active');
+    body.classList.toggle('lock');
+});
+
+});
