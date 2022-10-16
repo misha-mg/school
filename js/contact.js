4d3a132a71b766dc61af159c02b0426d4ce2a263
Mike <mishahorod@gmail.com>
2022-10-16 22:12:35 +0300

    little fix in contact

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    //  BURGER
+
+
+
+    const burger = document.querySelector('#header__burger'),
+    body = document.querySelector('body'),
+    menuNav = document.querySelector('#header-menu__navbar');
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      menuNav.classList.toggle('active');
+      body.classList.toggle('lock');
+    });
+
+    menuNav.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      menuNav.classList.toggle('active');
+      body.classList.toggle('lock');
+    });
+
+
+
       // FORMS
 
       const forms = document.querySelector('form'),
